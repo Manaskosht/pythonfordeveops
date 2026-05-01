28e6e7c5123f3623fcf71b0e0e71e6081dfe46f8
Manaskosht <koshtimanas@gmail.com>
2026-05-02 00:18:30 +0530

    casting

--- casting.py
+++ casting.py
@@ -2,7 +2,13 @@
 #print(type(var1))
 #print(var1)
 
-num1="50"
-num2="20"
-res=int(num1)+int(num2)
+#num1="50"
+#num2="20"
+#res=int(num1)+int(num2)
+#print(res)
+
+num1=50
+num2=20
+print(type(num1),type(num2))
+res=str(num1)+(str(num2))
 print(res)
