995498dcdb8923cea21ef711e6d199b76beb3f98
Manaskosht <koshtimanas@gmail.com>
2026-05-01 15:59:04 +0530

    manas

---
+++ first.py
@@ -0,0 +1,25 @@
+#1
+n1=20
+n2=30
+n3=n1
+n1=n2
+n2=n3
+print(f"n1={n1} n2={n2}")
+#2
+char1="hello"
+char2="java"
+char3=char1
+char1=char2
+char2=char3
+print(f"char1={char1} char2={char2}")
+#3
+n1=200
+char1="java"
+temp=n1
+n1=char1
+char1=temp
+print(f"n1={n1} char1={char1}")
+#5
+cur_balance=10000
+dep_balance=5000
+after
