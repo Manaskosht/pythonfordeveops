e71098135b422c74fdf69f345803d512276bcf1b
Manaskosht <koshtimanas@gmail.com>
2026-05-05 22:55:48 +0530

    practice

---
+++ 4-may.py
@@ -0,0 +1,71 @@
+#student_name="Manas koshti"
+#student_address="D-1 255/455 'NOIDA SEC-15' NEW DELHI 110096"
+#student_number="475554784497"
+#print(len(student_number))
+#student_profile="""hello my name is manas koshti 
+ #i am from "noida" and
+  #       i am a student of computer science engineering..."""
+#print(student_profile)
+#print(student_address)
+
+#Assignment opr.
+#a=10
+#a=a+10 #standard method
+#a+=10 #shorthand method
+#print(a)
+
+#3 Comparison opr.
+#n1=50
+#n2=100
+#res1=n1>n2
+#res2=n1<n2
+#print(res1)
+#print(res2)
+
+#char1='a'
+#char2='b'
+#res1=char1>char2
+#res2=char1<char2
+#print(res1)
+#print(res2)
+
+
+#<=,>=
+#n1=65
+#n2=85
+#res1=n1>=n2
+#res2=n1<=n2
+#print(res1)
+#print(res2)
+
+#n1=150
+#n2=110
+#res1=n1==n2
+#res2=n1!=n2
+#print(res1)
+#print(res2)
+
+#str1="hello"
+#str2="HELLO"
+#print(str1>str2)
+
+#str1="Fa"
+#str2="fa"
+#print(str1>str2)
+
+
+#4. Logical opr.
+#n1=50
+#n2=100
+#res=n1==50 and n2==10
+#print(res)
+
+#n1=25
+#n2=15
+#res=n1==25 or n2==100
+#print(res)
+
+n1=10
+n2=20
+res=not(n1==1 or n2==20 or n1==10 or n2==10)
+print(res)
---
+++ 5_may.py
@@ -0,0 +1,9 @@
+#membership operator
+#str1="this is python programming"
+#find="python"
+#print(find in str1)
+
+num1=4562
+print("4" not in num1)
+print("5" not in num1)
+
