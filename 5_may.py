6f4e38d8944dfa51f49cf97f81e36ee6e547c162
Manaskosht <koshtimanas@gmail.com>
2026-05-05 23:11:08 +0530

    update practice

--- 5_may.py
+++ 5_may.py
@@ -3,7 +3,24 @@
 #find="python"
 #print(find in str1)
 
-num1=4562
-print("4" not in num1)
-print("5" not in num1)
+#num1="4562"
+#print("4" not in str(num1))
+#print("5" not in str(num1))
 
+#email="koshti@gmail.com"
+#find="gmail"
+#print(find in email)
+#if find in email:
+ #   print("valid email")
+#else:
+ #   print("invalid email")
+
+#user is eligible for voting or not
+min_age=18
+nationality="indian"
+user_age=int(input("enter your age: "))
+user_id=input("enter your id: ")
+if min_age>=18:
+   print("eligible for voting")
+else:
+    print("not eligible for voting")
