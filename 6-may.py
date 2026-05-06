a3c10f7b26a3a52868e6ae481273f886abe9ed37
Manaskosht <koshtimanas@gmail.com>
2026-05-06 13:58:02 +0530

    update-practice

---
+++ 6-may.py
@@ -0,0 +1,54 @@
+#identity opr(is , is not)
+
+#a=10
+#b=32
+#print (id(a))
+#print (id(b))
+#print(a is b)
+#print(a is not b)
+
+
+
+
+#conditional statement
+#a=0
+#if a:
+ #   print("true")
+#else:
+ #   print("false")
+
+
+#a=True
+#b=True
+#print(a*b)
+
+
+#wap tp print the last digit of and number is 456735
+
+#num=456735
+#print(num%1000000)
+
+#print(6570025%10)
+
+#num1=3
+#num2=9
+#num3=12
+#wap to find largest
+#if num1>=num2 and num1>=num3:
+ #  print(num1)
+#if num2>=num1 and num2>=num3:
+ #  print(num2)
+#if num3>=num1 and num3>=num2:
+ #  print(num3)   
+
+ #condtional statements;
+ 
+
+#a="manas"
+#b=20
+#print (id(a))
+#print (id(b))
+#print(a is b)
+#print(a is not b)
+
+ 
