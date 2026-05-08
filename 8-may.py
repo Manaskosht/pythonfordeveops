271f2a11e13967e651ec220e07541be86d7006be
Manaskosht <koshtimanas@gmail.com>
2026-05-08 11:14:48 +0530

    question update

---
+++ 8-may.py
@@ -0,0 +1,27 @@
+#loops in python
+#1. for loop :ranged based
+#2. while loop :condition based
+
+#1.for loop
+#range always works withinteger number.
+#range(start:0, stop-1 , step-1)
+
+#for i in range(1,15,3):
+ #print(i)
+
+ #wap to takes start point and end point from user input and print all number divisible by 2 and 3
+
+ #wap to take a number from user input and print formatted table.
+ #format :
+ #3*1=3
+ #3*2=6
+
+ #wap to take a number from user input and print reversed formated table.
+
+range_start = int(input("enter start point : "))
+range_end = int(input("enter end point : ")) 
+for i in range(range_start ,range_end):
+   if i % 2 == 0 and i % 3 == 0 :
+    print(i)
+
+
