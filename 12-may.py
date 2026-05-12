b8801726f4d68e5004a3e577e4dbc20363156f9c
Manaskosht <koshtimanas@gmail.com>
2026-05-12 16:20:07 +0530

    practice update

---
+++ 12-may.py
@@ -0,0 +1,47 @@
+#traversing of string using range()
+
+# name="python"
+# size=len(name)
+# for i in range(size):
+#     print(name)
+
+# #without range()
+# name="python"
+# for i in name:
+#     print(i)
+
+# var1="Devops Engineer"
+# for i in var1:
+#     if i =="e"
+#       continue
+#     print(i,end=" ")
+
+#wap to count all the vowels from given string : "this is devops batch".
+
+# var1="this is devops batch"
+# v_count=0
+# c_count=0
+# for i in var1:
+#     if i in "aeiou":
+#         v_count+=1
+#     else:
+#         c_count+=1
+# print(v_count)
+# print(c_count)
+
+#wap to print your name in reverse format
+
+# name="manas"
+# reverse=""
+# for i in name:
+#     reverse=i+reverse
+# print(reverse) 
+      
+# name="python"
+# size=len(name)
+# for i in range(size):
+#     print(name[i],name,i)
+         
+
+
+
